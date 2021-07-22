6f619fff128e8f53c807686cce2b9ad5a606ad59
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-07-22 13:36:24 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -20,7 +20,7 @@ class Config(object):
     DOWNLOAD_LOCATION = "./DOWNLOADS"
 
     # Telegram maximum file upload size
-    TG_MAX_FILE_SIZE = 2097152000
+    TG_MAX_FILE_SIZE = 4194304000
 
     # chunk size that should be used with requests
     CHUNK_SIZE = 128
